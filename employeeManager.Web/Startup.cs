86f0a22bdfc9d25faf5c76e4c48c4ba21551261f
BrentLabasan <brentlabasan@gmail.com>
2015-12-11 13:10:26 -0800

    added a test section in the Razor view

---
+++ employeeManager.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(employeeManager.Web.Startup))]
+namespace employeeManager.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
